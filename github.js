5913c9cc0f927015cb39b79ad181025668e992b9
Ivar Abrahamsen <ivar@flurdy.com>
2017-05-09 14:20:11 +0100

    Cross domain json attempt 1 of 99

--- github.js
+++ github.js
@@ -1,14 +1,20 @@
+var recentRepos = [];
+
 function findRecentRepositories(response) {
-   $(response.data.filter(function(index){
-      return !index.fork;
-   }).slice(0,6))
-     .each(function(){
-      $("#recent-list ul.repolist")
-         .append("<li class=\"list-group-item\"><a href=\"" + this.html_url + "\">" +  this.name + "</a></li>");
+   recentRepos = $(response.data.filter(function(index){
+         return !index.fork;
+      }).slice(0,6));
+}
+
+function appendToRecent(repos){
+   $(repos).each(function(){
+    $("#recent-list ul.repolist")
+      .append("<li class=\"list-group-item\"><a href=\"" + this.html_url + "\">" +  this.name + "</a></li>");
    });
    $("#recent-list").removeClass("recent-hidden");
 }
 
 $(function() {
-   jQuery.getJSON("http://api.github.com/users/flurdy/repos?sort=pushed&callback=?",findRecentRepositories);
+   if(recentRepos.length > 0) appendToRecent(recentRepos)
+   else console.log("No repos found");
 });
